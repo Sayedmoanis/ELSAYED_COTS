256a3cd5a3e1feb04ffa5a8c7e0ef8df03078d03
ELSAYED MOANIS <sayedmoanis@gmail.com>
2023-11-25 23:42:49 +0200

    Adding initial library files

---
+++ Library/BIT_MATH.h
@@ -0,0 +1,4 @@
+#define SET_BIT(VAR,BIT_NUM) VAR|=(1<<BIT_NUM)
+#define CLR_BIT(VAR,BIT_NUM) VAR&=~(1<<BIT_NUM)
+#define TOG_BIT(VAR,BIT_NUM) VAR^=(1<<BIT_NUM)
+#define GET_BIT(VAR,BIT_NUM) ((VAR>>BIT_NUM)&1)
